86bf2c1ee5c07768a259ea412eff24a1104eb661
Sreenivasnaru <Sreenunaru672@gmail.com>
2024-02-01 20:49:51 +0530

    chcat app

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const { chats } = require("./data/data");
+const connectDB = require("./config/db");
+const colors = require("colors");
+
+const app = express();
+dotenv.config();
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("API is running successfully");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(5000, console.log(`server started on PORT ${PORT}`.yellow.bold));
